6f32f8d13eef4182c5faf71c06459fb22f3f67e8
GuyWSpecs <35390717+GuywithSpectacles@users.noreply.github.com>
2025-01-23 07:32:35 +0500

    Code for local mongodb establishment

--- backend/database/index.js
+++ backend/database/index.js
@@ -12,3 +12,36 @@ const dbConnect = async () => {
 }
 
 module.exports = dbConnect;
+
+
+/*
+const mongoose = require('mongoose');
+const { MONGODB_CONNECTION_STRING, NODE_ENV } = require('../config/index');
+
+const dbConnect = async () => {
+  const env = NODE_ENV || 'development'; // Default to 'development' if NODE_ENV is not set
+
+  try {
+    mongoose.set('strictQuery', false);
+
+    if (env === 'production') {
+      await mongoose.connect(MONGODB_CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log(`Connected to Database: ${env}`);
+    } else {
+      await mongoose.connect('mongodb://127.0.0.1:27017/CoinBounce', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log(`Connected to Database: ${env}`);
+    }
+  } catch (err) {
+    console.error("Not Connected to Database ERROR!", err);
+  }
+};
+
+module.exports = dbConnect;
+
+*/
